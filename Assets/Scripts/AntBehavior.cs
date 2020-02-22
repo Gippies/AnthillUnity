1599d5a709d7cc8668981fc937316dc082d05899
Joshua Patterson <jleegippies@gmail.com>
2020-02-21 19:26:13 -0600

    Added manual movement to ant

--- Assets/Scripts/AntBehavior.cs
+++ Assets/Scripts/AntBehavior.cs
@@ -4,15 +4,27 @@ using UnityEngine;
 
 public class AntBehavior : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private readonly float speed = 6f;
+
+    private Vector3 velocity;
+    private Rigidbody myRigidbody;
+
     void Start()
     {
-        
+        myRigidbody = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Vector3 input = new Vector3(Input.GetAxisRaw("Horizontal"), 0, Input.GetAxisRaw("Vertical"));
+        Vector3 direction = input.normalized;
+        velocity = direction * speed;
+    }
+
+    void FixedUpdate()
+    {
+        // Note that deltaTime here automatically recognizes it's inside of FixedUpdate
+        myRigidbody.position += velocity * Time.deltaTime;
     }
 }
