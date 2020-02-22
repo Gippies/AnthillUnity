3fa03e5e8dd5461001c2992344a6283fa2eac083
Joshua Patterson <jleegippies@gmail.com>
2020-02-21 22:02:46 -0600

    Added ant behavior of searching and (trying to) get leafies

--- Assets/Scripts/AntBehavior.cs
+++ Assets/Scripts/AntBehavior.cs
@@ -2,25 +2,35 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum State {
+    SEARCH,
+    GET_THING,
+    RETURN_TO_HILL,
+    RETURN_TO_SURFACE,
+    DROP_THING
+}
+
 public class AntBehavior : MonoBehaviour {
-    private static readonly float MAX_SEARCH_RADIUS = 10.0f;
+    private static readonly float MAX_SEARCH_RADIUS = 1.0f;
     private static readonly float MAX_SEARCH_SECONDS = 0.5f;
 
     private readonly float speed = 2.0f;
 
     private float searchSeconds;
+    private GameObject approaching;
+    private State currentState;
     private Vector3 velocity;
     private Rigidbody myRigidbody;
 
     void Start() {
         myRigidbody = GetComponent<Rigidbody>();
         searchSeconds = Random.Range(0.0f, MAX_SEARCH_SECONDS);
+        currentState = State.SEARCH;
+        approaching = null;
     }
 
-    // Update is called once per frame
-    void Update() {
+    private void Search() {
         searchSeconds -= Time.deltaTime;
-
         if (searchSeconds <= 0.0f) {
             Vector3 direction = new Vector3(Random.Range(-1.0f, 1.0f), 0, Random.Range(-1.0f, 1.0f)).normalized;
             velocity = direction * speed;
@@ -30,9 +40,23 @@ public class AntBehavior : MonoBehaviour {
             if (transform.position.x - MAX_SEARCH_RADIUS <= leafy.transform.position.x && leafy.transform.position.x <= transform.position.x + MAX_SEARCH_RADIUS &&
                 transform.position.y - MAX_SEARCH_RADIUS <= leafy.transform.position.y && leafy.transform.position.y <= transform.position.y + MAX_SEARCH_RADIUS &&
                 transform.position.z - MAX_SEARCH_RADIUS <= leafy.transform.position.z && leafy.transform.position.z <= transform.position.z + MAX_SEARCH_RADIUS) {
-                // Pick up leafy.
+                approaching = leafy;
+                currentState = State.GET_THING;
             }
         }
+    }
+
+    private void GetThing() {
+        Vector3 direction = (approaching.transform.position - transform.position).normalized;
+        velocity = direction * speed;
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (currentState == State.SEARCH)
+            Search();
+        else if (currentState == State.GET_THING)
+            GetThing();
     }
 
     void FixedUpdate()
