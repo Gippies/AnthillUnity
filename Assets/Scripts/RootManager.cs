fb652e6fb48fe2b09a7f7a2428ab156d82360f6f
Joshua Patterson <jleegippies@gmail.com>
2020-02-28 11:13:53 -0600

    Added ground made of cubes instead of a plane

--- Assets/Scripts/RootManager.cs
+++ Assets/Scripts/RootManager.cs
@@ -6,14 +6,25 @@ public class RootManager : MonoBehaviour {
 
     private static readonly int INITIAL_AMOUNT_OF_GATHERERS = 42;
     private static readonly int INITIAL_AMOUNT_OF_LEAVES = 100;
+    private static readonly int GROUND_WIDTH = 100;
+    private static readonly int GROUND_LENGTH = 100;
 
     public GameObject gathererPrefab;
     public GameObject leafyPrefab;
+    public GameObject groundPrefab;
 
     private List<GameObject> leafies;
 
     public List<GameObject> GetLeafies() {
         return leafies;
+    }
+
+    private void GroundSetup() {
+        for (int i = 0; i < GROUND_WIDTH; i++) {
+            for (int j = 0; j < GROUND_LENGTH; j++) {
+                Instantiate(groundPrefab, new Vector3(i - GROUND_WIDTH / 2, (float) -0.5, j - GROUND_LENGTH / 2), Quaternion.identity);
+            }
+        }
     }
 
     private void AntSetup() {
@@ -38,6 +49,7 @@ public class RootManager : MonoBehaviour {
 
     // Start is called before the first frame update
     void Start() {
+        GroundSetup();
         AntSetup();
         PlantSetup();
     }
